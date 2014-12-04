9234ff264333c2d8e66aef589f048d4627df5bb0
Shapes <mutantjessica@gmail.com>
2014-12-04 14:36:17 -0800

    add blockchain_api.json and qrcode.py.

---
+++ qrcode/qrcode.py
@@ -0,0 +1,15 @@
+####Create a QR code#####
+import qrcode
+import json
+from pprint import pprint
+
+with open('blockchain_api.json') as json_data:
+	data = json.load(json_data)
+pprint(data)
+data["address"]["1AJbsFZ64EpEfS5UAjAfcUG8pH8Jn3rn1F"]
+qr = qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,
+		box_size=10, border=4)
+qr.add_data('address')
+qr.make(fit=True)
+
+img = qr.make_image()
