8b2c04b6754a1b9eb5db20867fdb4b214c1c0984
Shapes <mutantjessica@gmail.com>
2014-12-10 13:37:14 -0800

    add receive module

---
+++ receive.py
@@ -0,0 +1,4 @@
+###blockchain python api recieve module: https://github.com/blockchain/api-v1-client-python/blob/master/docs/receive.md###
+from blockchain import receive
+
+response = receive.receive('input_address : str','destination_address : str')
